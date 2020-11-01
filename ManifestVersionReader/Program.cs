77a44cb4cde2fa23f51ca13a8cde8e39eaf0b6c7
mattire <alr2d4@hotmail.com>
2020-11-01 18:09:20 +0200

    init

---
+++ ManifestVersionReader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml;
+
+namespace ManifestVersionReader
+{
+    class Program
+    {
+        public static string ReadVersion(string path)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            var node = doc.SelectSingleNode("/manifest");
+            return node.Attributes.GetNamedItem("android:versionName").Value;
+        }
+
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            var versin = ReadVersion(path);
+            Console.WriteLine(versin);
+        }
+    }
+}
---
+++ ManifestVersionReader/test.cmd
@@ -0,0 +1,3 @@
+﻿echo off
+for /f %%i in ('ManifestVersionReader.exe "C:\Users\Matti\Documents\Visual Studio 2017\Projects\DrroidApp\DrroidApp\Properties\AndroidManifest.xml"') do set VAR=%%i
+echo %VAR%
